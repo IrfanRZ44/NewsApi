468ab88eefd61d6516cd2bbf52905003d5a4d86f
IrfanRZ44 <irfan.rozal44@gmail.com>
2021-06-17 11:26:34 +0800

    * Change tab style

--- app/src/main/java/id/telkomsel/merchant/ui/admin/AdminFragment.kt
+++ app/src/main/java/id/telkomsel/merchant/ui/admin/AdminFragment.kt
@@ -39,28 +39,38 @@ class AdminFragment : BaseFragmentBind<FragmentAdminBinding>() {
     @Suppress("DEPRECATION")
     private fun setupViewPager(pager: ViewPager) {
         val adapter = SectionsPagerAdapter(childFragmentManager)
-        adapter.addFragment(Blank1Fragment(), Constant.appName)
+        adapter.addFragment(Blank1Fragment(), "Merchant")
+        adapter.addFragment(Blank1Fragment(), "Produk")
         adapter.addFragment(AccountAdminFragment(), Constant.akun)
 
         pager.adapter = adapter
 
         bind.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                bind.tabs.getTabAt(0)?.icon = resources.getDrawable(R.drawable.ic_profile_black)
-                bind.tabs.getTabAt(1)?.icon = resources.getDrawable(R.drawable.ic_profile_gray)
+                bind.tabs.getTabAt(0)?.icon = resources.getDrawable(R.drawable.ic_multipeople_gray)
+                bind.tabs.getTabAt(1)?.icon = resources.getDrawable(R.drawable.ic_trolley_gray)
+                bind.tabs.getTabAt(2)?.icon = resources.getDrawable(R.drawable.ic_profile_gray)
                 supportActionBar?.title = Constant.request
                 supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
                 when (tab.position) {
                     0 -> {
                         supportActionBar?.title = Constant.appName
-                        bind.tabs.getTabAt(0)?.icon?.setColorFilter(resources.getColor(R.color.white), PorterDuff.Mode.SRC_IN)
-                        bind.tabs.getTabAt(1)?.icon?.setColorFilter(resources.getColor(R.color.gray2), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(0)?.icon?.setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(1)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(2)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
+                    }
+                    1-> {
+                        supportActionBar?.title = Constant.akun
+                        bind.tabs.getTabAt(0)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(1)?.icon?.setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(2)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
                     }
                     else -> {
                         supportActionBar?.title = Constant.akun
-                        bind.tabs.getTabAt(0)?.icon?.setColorFilter(resources.getColor(R.color.gray2), PorterDuff.Mode.SRC_IN)
-                        bind.tabs.getTabAt(1)?.icon?.setColorFilter(resources.getColor(R.color.white), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(0)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(1)?.icon?.setColorFilter(resources.getColor(R.color.gray1), PorterDuff.Mode.SRC_IN)
+                        bind.tabs.getTabAt(2)?.icon?.setColorFilter(resources.getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
                     }
                 }
             }
